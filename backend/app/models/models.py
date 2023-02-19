d81fc900e1caa77943531d5633f2bf81706d2049
naplot-hp <nadin45681@gmail.com>
2023-02-19 21:30:33 +0300

    roll back models

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,12 +1,37 @@
 from sqlalchemy import Column, String, Numeric, ForeignKey
 from sqlalchemy import text
-from sqlalchemy.orm import column_property
-from sqlalchemy import select, func
 
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+
+
+class Menu(Base):
+    __tablename__ = "menu"
+
+    id = Column(
+        String,
+        server_default=text("gen_random_uuid()"),
+        primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+
+class Submenu(Base):
+    __tablename__ = "submenu"
+
+    id = Column(
+        String,
+        server_default=text("gen_random_uuid()"),
+        primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    menu_id = Column(
+        String,
+        ForeignKey("menu.id", ondelete="CASCADE"),
+        nullable=False,
+    )
 
 
 class Dishes(Base):
@@ -28,49 +53,4 @@ class Dishes(Base):
         String,
         ForeignKey("submenu.id", ondelete="CASCADE"),
         nullable=False,
-    )
-
-
-class Submenu(Base):
-    __tablename__ = "submenu"
-
-    id = Column(
-        String,
-        server_default=text("gen_random_uuid()"),
-        primary_key=True)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    menu_id = Column(
-        String,
-        ForeignKey("menu.id", ondelete="CASCADE"),
-        nullable=False,
-    )
-    dishes_count = column_property(
-        select(func.count(Dishes.id))
-        .where(Dishes.submenu_id == id)
-        .correlate_except(Dishes)
-        .scalar_subquery()
-    )
-
-
-class Menu(Base):
-    __tablename__ = "menu"
-
-    id = Column(
-        String,
-        server_default=text("gen_random_uuid()"),
-        primary_key=True)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    submenus_count = column_property(
-        select(func.count(Submenu.id))
-        .where(Submenu.menu_id == id)
-        .correlate_except(Submenu)
-        .scalar_subquery()
-    )
-    dishes_count = column_property(
-        select(func.count(Dishes.id))
-        .where(Dishes.menu_id == id)
-        .correlate_except(Dishes)
-        .scalar_subquery()
     )
